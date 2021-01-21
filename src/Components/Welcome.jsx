9510e69e507e149a10922a2f7905384574dcec5e
jesus <Jesusfernandorios93@gmail.com>
2021-01-20 17:01:33 -0700

    integration of react-three fiber was a success. Need to style the text size and font so its more appealing

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,18 +1,57 @@
-import React from 'react'
-import './Style/Welcome.css'
-function Welcome() {
-    return (
-        <div className="Welcome">
-            
-            <div className="center__content">
-                <h1>WELCOME</h1>
-                <p>Let's Create Amazing Things Together</p>
-            </div>
+import React, { useRef, useState, useCallback } from 'react'
+import { Canvas } from 'react-three-fiber'
+import Effects from './Threejs/Effects'
+import {Html} from '@react-three/drei'
+import './Style/Welcome.css';
 
 
 
-        </div>
-    )
+function Ring() {
+  const mesh = useRef()
+  
+  return (
+    <mesh
+    ref={mesh}
+    >
+      <ambientLight intensity={3}/>
+      <ringBufferGeometry  args={[5,6,50]}/>
+      <meshStandardMaterial color="purple"/>
+      
+    </mesh>
+  )
 }
 
-export default Welcome
+
+function Welcome() {
+  const [down, set] = useState(false)
+  const mouse = useRef([0, 0])
+  const onMouseMove = useCallback(({ clientX: x, clientY: y }) => (mouse.current = [x - window.innerWidth / 2, y - window.innerHeight / 2]), [])
+
+  return (
+    <Canvas
+      
+      camera={{ fov: 100, position: [0, 0, 10] }}
+      onMouseMove={onMouseMove}
+      onMouseUp={() => set(false)}
+      onMouseDown={() => set(true)}
+    >
+      <ambientLight intensity={5}/>
+      <pointLight distance={100} intensity={4} color="white" />
+      <fog attach="fog" args={['white', 50, 190]} />
+      <Ring/>
+      <Html 
+        center
+      >
+        <div className="content">
+          <h1>Welcome</h1>
+          <p>Lets Create Amazing Things Together</p>
+        </div>
+      </Html>
+      <Effects down={down}/>
+      
+    </Canvas>
+    
+  );
+}
+
+export default Welcome;
