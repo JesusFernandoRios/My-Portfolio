115e80c028c2e7b925454ad27a06e146830af70c
jesus <Jesusfernandorios93@gmail.com>
2021-01-28 15:09:22 -0700

    created media queires for max width 768px

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,18 +5,18 @@ function Header() {
     return (
         <header className="top">
             <div className="print">
-                <div >
+                <div className="name__div">
                     <a className="name" href="#home">Jesus Fernando Rios</a>
                 </div>
                 <div className="title">Full Stack Developer</div>
             </div>
             <div className="navbar">
                 <ul className="links">
-                    <li><a id="welcome" href="#About">About |</a></li>
+                    <li><a id="welcome" href="#About">|About </a></li>
             
-                    <li><a id="welcome" href="#Projects">Portfolio </a></li>
+                    <li><a id="welcome" href="#Projects">|Portfolio </a></li>
                     
-                    <li><a id="welcome" href="#Contact">| Contact</a></li>
+                    <li><a id="welcome" href="#Contact">|Contact</a></li>
                 </ul>
             </div>
         </header>
